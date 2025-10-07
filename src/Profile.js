f431eae60ff5ee15bf26d984f90183e4ccbfcb41
Margarita <mmmareeva95@gmail.com>
2025-10-07 06:39:51 -0700

    Initial commit

---
+++ src/Profile.js
@@ -0,0 +1,8 @@
+export function Profile() {
+  return (
+    <div>
+      <h1>Восхитительные ученые</h1>
+      <img src="https://i.imgur.com/QIrZWGIs.jpg" alt="Алан Л. Харт" />
+    </div>
+  );
+}
